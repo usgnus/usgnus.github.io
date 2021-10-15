97aa56c232ed2f197470e13e6f767f25f2149241
usgnus <anago3820@daum.net>
2021-10-16 00:36:55 +0900

    create image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "usgnus",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
